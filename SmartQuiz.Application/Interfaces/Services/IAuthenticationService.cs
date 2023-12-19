4fc19a0b946ee878ec8086e5969f997232b2acfe
Kingkaspev <kingsleychiboy22@gmail.com>
2023-12-19 11:47:53 +0100

    IAuthenticationService

--- SmartQuiz.Application/Interfaces/Services/IAuthenticationService.cs
+++ SmartQuiz.Application/Interfaces/Services/IAuthenticationService.cs
@@ -2,5 +2,10 @@
 {
     public interface IAuthenticationService
     {
+        Task<ApiResponse<string>> ForgotPasswordAsync(string email);
+        Task<ApiResponse<string>> ResetPasswordAsync(string email, string token, string newPassword);
+        Task<ApiResponse<string>> ValidateTokenAsync(string token);
+        ApiResponse<string> ExtractUserIdFromToken(string authToken);
+        Task<ApiResponse<string>> ChangePasswordAsync(AppUser user, string currentPassword, string newPassword);
     }
 }

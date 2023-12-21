15e52a41cee5dfd59bb0f1981a3709f1aed0bf54
Gemma <jemmimah.abdul@decagon.dev>
2023-12-21 19:35:31 +0100

    implemented the Controller

--- SmartQuiz/Controllers/EducatorController.cs
+++ SmartQuiz/Controllers/EducatorController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartQuiz.Application.Interfaces.Services;
+using SmartQuiz.Application.ServicesImplementation;
+using SmartQuiz.Domain.Entities;
+using System.Reflection;
 
 namespace SmartQuiz.Controllers
 {
@@ -6,6 +10,44 @@ namespace SmartQuiz.Controllers
     [ApiController]
     public class EducatorController : ControllerBase
     {
+        private readonly IEducatorServices _services;
+        private readonly IQuizServices _quizServices;
+        private readonly ILogger _logger;
+
+        public EducatorController(IEducatorServices services, IQuizServices quizServices, ILogger logger)
+        {
+            _services = services;
+            _quizServices = quizServices;
+            _logger = logger;
+        }
+
+        [HttpGet("GetById")]
+
+        public  IActionResult GetQuizzesByEducatorId(string id)
+        {
+            var response =  _services.GetQuizzesByEducatorId(id); 
+            return Ok(response);
+        }
+
+        [HttpGet("GetQuizDetails")]
+        public IActionResult GetQuizDetails(string id) 
+        { 
+            var quiz = _services.GetQuizDetails(id);
+            
+            {
+                //id = quiz.Id
+                //QuizName = quiz.Title, // Map Title to QuizName in the ViewModel
+                //Description = quiz.Description,
+                
+            };
+            return Ok(quiz);
+        }
+
+        public IActionResult Delete(string quizId)
+        {
+            var isDeleted = _services.DeleteQuiz(quizId);
+            return Ok(isDeleted);
+        }
 
     }
 }

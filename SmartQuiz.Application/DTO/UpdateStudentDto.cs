57e810d431be1b673e259cbf7d1ef27ad9e5db1a
Chukky-27 <cartusky4eva88@gmail.com>
2023-12-23 15:07:01 +0100

    studentController

--- SmartQuiz.Application/DTO/UpdateStudentDto.cs
+++ SmartQuiz.Application/DTO/UpdateStudentDto.cs
@@ -12,11 +12,11 @@ namespace SmartQuiz.Application.DTO
         public string Address { get; set; }
         public string State { get; set; }
         public string Gender { get; set; }
-        public string ImageUrl { get; set; }
-        public string VerificationToken { get; set; }
-        public string PasswordResetToken { get; set; }
-        public DateTime? ResetTokenExpires { get; set; }
-        public DateTime? VerifiedAt { get; set; }
-        public ICollection<Quiz> Quizzes { get; set; }
+        //public string ImageUrl { get; set; }
+        //public string VerificationToken { get; set; }
+        //public string PasswordResetToken { get; set; }
+        //public DateTime? ResetTokenExpires { get; set; }
+        //public DateTime? VerifiedAt { get; set; }
+        //public ICollection<Quiz> Quizzes { get; set; }
     }
 }
--- SmartQuiz/Controllers/StudentController.cs
+++ SmartQuiz/Controllers/StudentController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartQuiz.Application.DTO;
+using SmartQuiz.Application.Interfaces.Services;
+using SmartQuiz.Domain;
 
 namespace SmartQuiz.Controllers
 {
@@ -6,5 +9,49 @@ namespace SmartQuiz.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly IStudentServices _studentServices;
+        private readonly ILogger<StudentController> _logger;              
+
+        public StudentController(IStudentServices studentServices, ILogger<StudentController> logger)
+        {
+            _studentServices = studentServices;
+            _logger = logger;
+        }
+
+        [HttpGet("GetAllStudents")]
+        public IActionResult GetAllStudents()
+        {
+            var response = _studentServices;
+            return Ok(response);
+        }
+
+        [HttpGet("{StudentId}")]
+        public IActionResult GetStudentById(string studentId)
+        {
+            var response = _studentServices.GetStudentByIdAsync(studentId);
+            if (response.Succeeded)
+            {
+                return Ok(response.Data);
+            }
+            return StatusCode(response.StatusCode, new {errors = response.Errors});
+        }
+
+        [HttpPut("UpdateStudent/{id}")]
+        public IActionResult UpdateStudent(string studentId, [FromBody] UpdateStudentDto updateStudentDto)
+        {
+            var updateStudent = _studentServices.UpdateStudentAsync(studentId, updateStudentDto);
+            if (updateStudent.Succeeded)
+            {
+                return Ok(new ApiResponse<bool>(true, "Student updated successfully.", 200, true, null));
+            }
+            _logger.LogError("Student update failed: {Message}", updateStudent.Message);
+            return BadRequest(new ApiResponse<bool>(false, "Failed to update student.", 400, false, updateStudent.Errors));
+        }
+
+        [HttpDelete("DeleteStudentById")]
+        public async Task<IActionResult> DeleteStudentById(string id)
+        {
+            return Ok(await _studentServices.DeleteStudentAsync(id));
+        }
     }
 }
